963267b9300f449a3e318dfc0036b551f1d150f7
Toshiya Nakakura <t.nakakura@ntt.com>
2014-07-16 22:42:46 +0900

    prob20

--- src/main/scala/P01_20.scala
+++ src/main/scala/P01_20.scala
@@ -5,11 +5,7 @@ import java.util.Date
 
 object P01_20 {
   def main(args: Array[String]): Unit = {
-    val a06 = isPalindrome[Int](List(1, 2, 1))
-    val a08 = compress[Symbol](List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
-    val a09 = pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
-    val a10 = encode(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
-    println(a10)
+    println(removeAt(1, List('a, 'b, 'c, 'd)))
   }
 
   //P01 (*) Find the last element of a list.
@@ -75,10 +71,79 @@ object P01_20 {
     case x :: l => if(n == x.head) (n :: x) :: l else List(n) :: (x :: l)
   }).reverse
 
-  //pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+  //P10 (*) Run-length encoding of a list.
   def encode[A](list: List[A]): List[(Int, A)] = list.foldLeft(List.empty[(Int, A)])((z, n)=> z match {
     case Nil => (1, n) :: z
     case x :: l => if(n == x._2) (x._1 + 1, n) :: l else (1, n) :: (x :: l)
   }).reverse
 
+  //P11 (*) Modified run-length encoding.
+  def encodeModified[A](list: List[A]): List[Any] = {
+    encode(list).map(item => item match{
+      case (1, l) => l
+      case (_, l) => item
+    })
+  }
+
+  //P12 (**) Decode a run-length encoded list.
+  def decode[A](list: List[(Int, A)]): List[A] = {
+    def decodeItem[A](item: (Int, A)): List[A] = item match {
+      case (0, l) => Nil
+      case (n, l) => l :: decodeItem((n-1, l))
+    }
+    list.flatMap(item =>{
+      decodeItem(item)
+    })
+  }
+
+  //P13 (**) Run-length encoding of a list (direct solution).
+  def encodeDirect[A](list: List[A]): List[(Int, A)] = {
+    list.foldLeft(List.empty[(Int, A)])((z, n) => {
+      if(z.head._2 == n) (z.head._1 + 1, z.head._2) :: z.tail
+      else (1, n) :: z
+    })
+  }
+
+  //P14 (*) Duplicate the elements of a list.
+  def duplicate[A](list: List[A]): List[A] = list flatMap(item=>{
+    List(item, item)
+  })
+
+  //P15 (**) Duplicate the elements of a list a given number of times.
+  def deplicateN[A](times: Int, list: List[A]): List[A] = list.flatMap(item=>{
+    List.fill(times)(item)
+  })
+
+  //P16 (**) Drop every Nth element from a list.
+  def drop[A](n: Int, list: List[A]): List[A] = {
+    list.foldLeft((n, List.empty[A]))((z, x) => z match {
+      case (1, l) => (n, l)
+      case (k, l) => (k - 1, x :: l)
+    })._2.reverse
+  }
+
+  //P17 (*) Split a list into two parts.
+  def split[A](n: Int, list: List[A]): (List[A], List[A]) = {
+    (list.take(n), list.drop(n))
+  }
+
+  //P18 (**) Extract a slice from a list.
+  def slice[A](start: Int, end: Int, list: List[A]) : List[A] = {
+    list.drop(start).take(end - start)
+  }
+
+  //P19 (**) Rotate a list N places to the left.
+  def rotate[A](n: Int, list: List[A]): List[A] = {
+    if(n > 0) list.drop(n) ++ list.take(n)
+    else list.drop(list.length + n) ++ list.take(list.length + n)
+  }
+
+  def removeAt[A](n: Int, list: List[A]): (List[A], A) = {
+    val ret = list.zipWithIndex.foldLeft((List.empty[A], list.head))((z, item) =>{
+      if(item._2 == n) (z._1, item._1)
+      else (item._1 :: z._1, z._2)
+    })
+
+    (ret._1.reverse, ret._2)
+  }
 }
