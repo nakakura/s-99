3894365f23010996b5ae4acafd8784212d1d5788
Toshiya Nakakura <t.nakakura@ntt.com>
2014-10-06 11:13:02 +0900

    2014/10/6

--- src/main/scala/P31_60.scala
+++ src/main/scala/P31_60.scala
@@ -5,8 +5,25 @@
 class S99Int(val n: Int) {
   import S99Int._
 
+  //P31 (**) Determine whether a given integer number is prime.
   def isPrime: Boolean =
     (n > 1) && (primes takeWhile { _ <= math.sqrt(n) } forall { n % _ != 0 })
+
+  //P33 (*) Determine whether two positive integer numbers are coprime.
+  def isComprimeTo(x: Int): Boolean = {
+    if(S99Int.gcd(n, x) == 1) return true
+    else false
+  }
+
+  //P34 (**) Calculate Euler's totient function phi(m).
+  def totient: Int = {
+    (1 to n).filter(n.isComprimeTo(_)).length
+  }
+
+  //P35 (**) Determine the prime factors of a given positive integer.
+  def primeFactors : Int = {
+    (1 to n).filter(n.isComprimeTo(_)).length
+  }
 }
 
 object S99Int {
@@ -15,8 +32,16 @@ object S99Int {
   val primes = Stream.cons(2, Stream.from(3, 2) filter { _.isPrime })
 
   def main(args: Array[String]): Unit = {
-    for (n <- 1 to 10)
-      println(n + ": " + n.isPrime)
+    println(10.totient)
   }
+
+  //P32 (**) Determine the greatest common divisor of two positive integer numbers.
+  def gcd(m: Int, n: Int): Int = {
+    if(n == 0) return m
+    else if(m < n) return gcd(n, m)
+    gcd(n, m % n)
+  }
+
+
 }
 
